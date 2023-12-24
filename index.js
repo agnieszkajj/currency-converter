85f9f535408a7dc4c0ec87127c408f2cadc3c70b
Agnieszka <agnieszka.jjjjj@gmail.com>
2023-12-24 05:57:43 +0100

    add form and error handling

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const fetchData = (currencyCode) => {
-  fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${currencyCode}/today/`)
+const fetchData = () => {
+  fetch(
+    `http://api.nbp.pl/api/exchangerates/rates/A/${selectElement.value}/today/`
+  )
     .then((res) => res.json())
     .then((data) => {
-      const rate = data.rates[0].mid;
-      const sum = parseFloat(amount.value) * parseFloat(rate);
-      finalAmount.innerHTML = `${sum.toFixed(2)} PLN`;
-      amount.value = "";
-    });
+      const rate = data?.rates?.[0]?.mid;
+      if (rate) {
+        const sum = parseFloat(amount.value) * parseFloat(rate);
+        finalAmount.innerHTML = `${sum.toFixed(2)} PLN`;
+      } else {
+        alert("something went wrong");
+      }
+    })
+    .catch((error) => alert(error));
 };
 
 const amount = document.querySelector("#amount");
-const btn = document.querySelector("#btn");
+const form = document.querySelector("#form");
 const selectElement = document.querySelector("#currencyCode");
 const finalAmount = document.querySelector("#finalAmount");
 
-btn.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   if (amount.value > 0) {
-    fetchData(selectElement.value);
+    fetchData();
   } else {
     alert("Please enter a valid amount!");
   }
